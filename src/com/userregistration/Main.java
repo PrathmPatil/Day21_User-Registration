81c42165a3440a4c58d0b27331a1b838ccfe7375
PrathmPatil <prathmpatil2818@gmail.com>
2023-01-13 20:15:03 +0530

    Check last name are valid or not

---
+++ src/com/userregistration/Main.java
@@ -0,0 +1,11 @@
+package com.userregistration;
+
+public class Main {
+
+	public static void main(String[] args) {
+		User_Registration obj=new User_Registration();
+		//obj.firstName();
+		obj.lastName();
+	}
+
+}
--- src/com/userregistration/User_Registration.java
+++ src/com/userregistration/User_Registration.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class User_Registration {
 
-	public static void main(String[] args) {
+	void firstName() {
 		/*
 		 * Check First Name are valid or not
 		 */
@@ -24,5 +24,25 @@ public class User_Registration {
         	System.out.println(" Invalid.");
         }
 	}
+	void lastName() {
+        /*
+         * Check Last Name are valid or not
+         */
+		String lname;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter last name: ");
+		lname=sc.next();
+		boolean lName=Pattern.matches("[A-Z]{1}[a-zA-Z]{2}", lname);
+        System.out.print("Enterd Name "+lname+" are");
+        if(lName==true)
+        {
+        	System.out.println(" Valid.");
+        }
+        else
+        {
+        	System.out.println(" Invalid.");
+        }
+	}
+	
 
 }
